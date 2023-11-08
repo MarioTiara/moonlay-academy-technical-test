cf966608f09683c0313d01fae52b646cee877a40
MarioTiara <mario.tiarapratama@gmail.com>
2023-11-08 23:14:04 +0700

    add handler

--- handlers/task.go
+++ handlers/task.go
@@ -4,11 +4,23 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	task "github.com/marioTiara/todolistwebapi/Task"
 )
 
+type taskHandler struct {
+	taskService task.Service
+}
+
+func NewTaskHandler(taskService task.Service) *taskHandler {
+	return &taskHandler{taskService}
+}
 func RootHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"name": "Mario Tiara",
 		"bio":  "A Full Stack SOftware Enginner",
 	})
+}
+
+func (handler *taskHandler) TasksHandler(c *gin.Context) {
+
 }
