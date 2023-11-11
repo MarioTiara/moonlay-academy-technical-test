5e3d0c5ac32167ddc4a5d28479252725b5de7b25
MarioTiara <mario.tiarapratama@gmail.com>
2023-11-11 14:27:03 +0700

    add PostTaskHandler method

--- handlers/task.go
+++ handlers/task.go
@@ -1,9 +1,11 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
 	task "github.com/marioTiara/todolistwebapi/Task"
 )
 
@@ -21,6 +23,34 @@ func RootHandler(c *gin.Context) {
 	})
 }
 
-func (handler *taskHandler) TasksHandler(c *gin.Context) {
+func (h *taskHandler) PostTasksHandler(c *gin.Context) {
+	var taskRequest task.AddTaskRequest
+	err := c.ShouldBindJSON((&taskRequest))
+
+	if err != nil {
+		errorMessages := []string{}
+		for _, e := range err.(validator.ValidationErrors) {
+			errorMessage := fmt.Sprintf("Error on field %s, condition: %s", e.Field(), e.ActualTag())
+			errorMessages = append(errorMessages, errorMessage)
+		}
+
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": errorMessages,
+		})
+
+		return
+	}
+
+	task, err := h.taskService.Create(taskRequest)
+
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"errors": err,
+		})
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"data": task,
+	})
 
 }
